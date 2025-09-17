3b2e3953c946cd10a8cd9ba3bdd53aacd9660a78
AnRumyantseva <zacepurina.av@gmail.com>
2025-09-17 14:55:05 +0500

    5.7 Знакомство с TCP/IP протоколами2

--- tcp_server.py
+++ tcp_server.py
@@ -4,20 +4,21 @@ import socket
 def server():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    server_address = ('localhost', 20215)
+    server_address = ('localhost', 20216)
     server_socket.bind(server_address)
 
-    server_socket.listen(5)
+    server_socket.listen(10)
     print("Server started and wait connections ....")
 
+    messages: list[str] = []
     while True:
         client_socket, client_address = server_socket.accept()
         print(f"Connection from {client_address}")
 
-        data = client_socket.recv(1024).decode()
-        print(f"Received message: {data}")
-
-        response = f"Server received : {data}"
+        message = client_socket.recv(1024).decode()
+        messages.append(message)
+        print(f"Received message: {messages}")
+        response = f"Server received : {messages}"
 
         client_socket.send(response.encode())
         client_socket.close()

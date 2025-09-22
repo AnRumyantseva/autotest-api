01ab6344ecb8b43ccc05356b3b0f6ec9cf226f05
AnRumyantseva <zacepurina.av@gmail.com>
2025-09-22 11:14:07 +0500

    6.1 Введение в HTTPX v2

--- httpx_authentication.py
+++ httpx_authentication.py
@@ -19,3 +19,8 @@ response_refresh_data = response_login.json()
 print("Refresh response:", response_refresh)
 print("Status Code:", response_refresh.status_code)
 
+
+client = httpx.Client(headers = {"Authorization" : f"Bearer {response_login_data['token']['accessToken']}"})
+response_me = client.get("http://localhost:8000/api/v1/users/me")
+print("Me response:", response_me.json())
+print("Me Code:", response_me.status_code)

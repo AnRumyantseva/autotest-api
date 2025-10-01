451a601d5485799bd00f0d6e21c0f82bf6b3288b
AnRumyantseva <zacepurina.av@gmail.com>
2025-10-01 15:24:10 +0500

    7.2 Применяем Pydantic в API клиентах v2

--- api_client_create_exercise.py
+++ api_client_create_exercise.py
@@ -1,28 +1,32 @@
 from clients.courses.courses_client import get_courses_client, CreateCourseRequestDict
 from clients.exercises.exercises_client import get_exercises_client, CreateExerciseRequestDict
-from clients.files.files_client import get_files_client, CreateFileRequestDict
+from clients.files.files_client import get_files_client
 from clients.private_http_builder import AuthenticationUserSchema
-from clients.users.public_users_client import get_public_users_client, CreateUserRequestDict
+from clients.users.public_users_client import get_public_users_client
+from clients.users.users_schema import CreateUserRequestSchema
+from clients.files.files_schema import CreateFileRequestSchema
+
+
 from tools.fakers import get_random_email
 
 public_users_client = get_public_users_client()
-create_user_request = CreateUserRequestDict(
+create_user_request = CreateUserRequestSchema(
     email=get_random_email(),
     password="string123",
-    lastName="string123",
-    firstName="string123",
-    middleName="string123"
+    last_name="string123",
+    first_name="string123",
+    middle_name="string123"
 )
 
 create_user_response = public_users_client.create_user(create_user_request)
 authentication_user = AuthenticationUserSchema(
-    email=create_user_request['email'],
-    password=create_user_request['password']
+    email=create_user_request.email,
+    password=create_user_request.password
 )
 file_client = get_files_client(authentication_user)
 course_client = get_courses_client(authentication_user)
 
-create_file_request = CreateFileRequestDict(
+create_file_request = CreateFileRequestSchema(
     filename="pick.jpg",
     directory="courses",
     upload_file='./testdata/files/pick.jpg'
@@ -36,8 +40,8 @@ create_course_request = CreateCourseRequestDict(
     minScore=10,
     description="Python API course",
     estimatedTime="2 weeks",
-    previewFileId=create_file_response['file']['id'],
-    createdByUserId=create_user_response['user']['id']
+    previewFileId=create_file_response.file.id,
+    createdByUserId=create_user_response.user.id
 )
 create_course_response = course_client.create_course(create_course_request)
 print('Create course data:', create_course_response)

f10652f399ac9b3c90c484f980c3a20f6bfcbd54
AnRumyantseva <zacepurina.av@gmail.com>
2025-10-03 15:40:47 +0500

    7.4 Знакомство с Faker v2

--- api_client_create_course.py
+++ api_client_create_course.py
@@ -5,16 +5,9 @@ from clients.users.public_users_client import get_public_users_client
 from clients.users.users_schema import CreateUserRequestSchema
 from clients.files.files_schema import CreateFileRequestSchema
 from clients.courses.courses_schema import CreateCourseRequestSchema
-from tools.fakers import fake
 
 public_users_client = get_public_users_client()
-create_user_request = CreateUserRequestSchema(
-    email=fake.email(),
-    password="string123",
-    last_name="string123",
-    first_name="string123",
-    middle_name="string123"
-)
+create_user_request = CreateUserRequestSchema()
 
 create_user_response = public_users_client.create_user(create_user_request)
 authentication_user = AuthenticationUserSchema(
@@ -24,20 +17,11 @@ authentication_user = AuthenticationUserSchema(
 file_client = get_files_client(authentication_user)
 course_client = get_courses_client(authentication_user)
 
-create_file_request = CreateFileRequestSchema(
-    filename="pick.jpg",
-    directory="courses",
-    upload_file='./testdata/files/pick.jpg'
-)
+create_file_request = CreateFileRequestSchema(upload_file='./testdata/files/pick.jpg')
 create_file_response = file_client.create_file(create_file_request)
 print('Create file data:', create_file_response)
 
-create_course_request= CreateCourseRequestSchema(
-    title="Python",
-    max_score=100,
-    min_score=10,
-    description="Python API course",
-    estimated_time="2 weeks",
+create_course_request = CreateCourseRequestSchema(
     preview_file_id=create_file_response.file.id,
     created_by_user_id=create_user_response.user.id
 )

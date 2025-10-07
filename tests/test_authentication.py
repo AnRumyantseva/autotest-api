4973f625d7eb8027573933e530582c5f99ccb3fa
AnRumyantseva <zacepurina.av@gmail.com>
2025-10-07 22:20:47 +0500

    8.5 Фикстуры Pytest v3

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,8 +2,6 @@ import pytest
 from http import HTTPStatus
 from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema
 from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.schema import validate_json_schema

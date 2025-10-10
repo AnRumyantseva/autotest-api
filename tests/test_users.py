7319752137a38fe28ff35799ffa5d81e894aa148
AnRumyantseva <zacepurina.av@gmail.com>
2025-10-10 11:21:35 +0500

    8.7 Параметризация Pytest v2

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,6 +4,7 @@ from clients.users.users_schema import CreateUserRequestSchema, CreateUserRespon
 from http import HTTPStatus
 import pytest
 
+from tools.fakers import fake
 from fixtures.users import UserFixture
 from tools.assertions.schema import validate_json_schema
 from tools.assertions.basic import assert_status_code
@@ -12,8 +13,9 @@ from tools.assertions.users import assert_create_user, assert_get_user_response
 
 @pytest.mark.users
 @pytest.mark.regression
-def test_create_user(public_users_client: PublicUsersClient):
-    request = CreateUserRequestSchema()
+@pytest.mark.parametrize("email", ["mail.ru", "gmail.com", "example.com"])
+def test_create_user(email: str,public_users_client: PublicUsersClient):
+    request = CreateUserRequestSchema(email=fake.email(domain=email))
     response = public_users_client.create_user_api(request)
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
 

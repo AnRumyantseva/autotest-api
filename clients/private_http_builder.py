559fb406afe303c06417a895945c6fc67479e8e0
AnRumyantseva <zacepurina.av@gmail.com>
2025-10-22 15:22:30 +0500

    11.1 Настройки автотестов с Pydantic

--- clients/private_http_builder.py
+++ clients/private_http_builder.py
@@ -4,20 +4,21 @@ from clients.authentication.authentication_client import get_authentication_clie
 from clients.authentication.authentication_schema import LoginRequestSchema
 from pydantic import BaseModel
 from functools import lru_cache
-
 from clients.event_hooks import curl_event_hooks
+from config import settings
 
 
 class AuthenticationUserSchema(BaseModel, frozen=True):
     email: str
     password: str
 
+
 @lru_cache(maxsize=None)
 def get_private_http_client(user: AuthenticationUserSchema) -> Client:
     authentication_client = get_authentication_client()
-    login_request = LoginRequestSchema(email= user.email, password = user.password)
+    login_request = LoginRequestSchema(email=user.email, password=user.password)
     login_response = authentication_client.login(login_request)
-    return Client(timeout=100,
-                  base_url="http://localhost:8000",
+    return Client(timeout=settings.http_client.timeout,
+                  base_url=settings.http_client.client_url,
                   headers={"Authorization": f"Bearer {login_response.token.access_token}"},
                   event_hooks={"request": [curl_event_hooks]})
